7747550dc3679af60eee054a253d29d819ee3ba9
evertonrwr <evertonrwrocha@gmail.com>
2022-11-08 20:13:41 -0300

    Fix-CCS

--- front/src/Components/produtos/cadastroClienteProduto.tsx
+++ front/src/Components/produtos/cadastroClienteProduto.tsx
@@ -1,79 +1,129 @@
-import React, {Component} from "react";
+import React, {useState, useEffect} from "react";
 import BarraNavegacao from "../barraNavegacao";
 import "../../Css/clientes.css"
 import { Link } from "react-router-dom";
-class CadastroClienteProduto extends Component{
-    componentDidMount() {
-            let select = document.querySelectorAll('.select');
-            M.FormSelect.init(select)
-    }
+import { textChangeRangeIsUnchanged } from "typescript";
+import { render } from "@testing-library/react";
+
+const  CadastroClienteProduto = () => {
     
-    render(){
-        var botoes = [{valor: "ClIENTES", link:"/Clientes"}, {valor: "SERVIÇOS", link:"/servicos"},{valor: "PRODUTOS", link:"/produtos"} ]
-        let barraNavegacao = <BarraNavegacao  tema="purple accent-4" botoes={botoes} />;
-        return(
-            <div>
-               {barraNavegacao}
-              
-               <div className="container">
-                    <div className="row">
-                        <div className="col s12 ">
-                            <div className="card ">
-                                <div className="card-content ">
-                                <h1 className="card-title">CADASTRAR CLIENTE/PRODUTO</h1>
-                                <hr></hr>
-                                <div className="card-body">
-                                    <div className="row">
-                                        <form className="col s12">
+    var cliente =[{Id:0, Nome:""}]
+    var servico =[{Id:0, Nome:""}]
+    const [clientes, setClientes] = useState(cliente)
+    const [servicos, setServicos] = useState(servico)
+    
+    var listarServicos = () =>{
+        fetch("/listarServico", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+        }).then((res)=> res.json()).then((data)=>{
+            setServicos(data)
+        })
+    }
+    var listarClientes =() =>{
+        fetch("/listarClientes", {
+            method: "get",
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+        }).then((res)=> res.json()).then((data)=>{
+           setClientes(data )
+        })
+    }
+   var clickMe = function (e:any){
+    e.preventDefault();
+    var Id = document.getElementById("servico") as HTMLSelectElement
+   var index =  servico.indexOf({
+        Id: parseInt(Id.value) ,
+        Nome: Id.textContent? Id.textContent: ""
+    } )
+    console.log(servicos[index])
+   }
+    useEffect(()=>{
+        let select = document.querySelectorAll('.select');
+        M.FormSelect.init(select);
+         listarClientes();
+         listarServicos();
+         setTimeout(() => {
+            let select = document.querySelectorAll('.select');
+            M.FormSelect.init(select);
+            }, 1000);
+    }, [])
+    
+      
+    
+    
+    var botoes = [{valor: "ClIENTES", link:"/Clientes"}, {valor: "SERVIÇOS", link:"/servicos"},{valor: "PRODUTOS", link:"/produtos"} ]
+    let barraNavegacao = <BarraNavegacao  tema="purple accent-4" botoes={botoes} />;
+    return(
+        <div>
+            {barraNavegacao}
+            
+            <div className="container">
+                <div className="row">
+                    <div className="col s12 ">
+                        <div className="card ">
+                            <div className="card-content ">
+                            <h1 className="card-title">CADASTRAR CLIENTE/PRODUTO</h1>
+                            <hr></hr>
+                            <div className="card-body">
+                                <div className="row">
+                                    <form className="col s12">
+                                    
+                                        <div className="row">
+                                            <div className="input-field col s12">
+                                                <select id="servico" className="select">
+                                                <option value="" disabled selected>Selecione..</option>
+                                                {servicos.map(function (a) {
+                                                    return <option value={a.Id}> {a.Nome}</option>
+                                                })};
+                                            
+                                                </select>
+                                            
+                                                <label htmlFor="servico">Serviço</label>
+                                            </div>
                                         
+                                        </div> 
                                         <div className="row">
-                                        <div className="input-field col s12">
-                                            <select id="produto" className="select">
-                                            <option value="" disabled selected>Selecione..</option>
-                                            <option className="option"value="1">Produto 1</option>
-                                            <option className="option" value="2">Produto 2</option>
-                                            </select>
-                                            <label htmlFor="produto">Produtos</label>
+                                        
+                                            <div className="input-field col s12">
+                                                <select id="cliente" className="select">
+                                                <option value="" disabled selected>Selecione..</option>
+                                                {clientes.map(function (a) {
+                                                    return <option value={a.Id}> {a.Nome}</option>
+                                                })};
+                                                </select>
+                                                <label htmlFor="cliente">Cliente</label>
+                                            </div>
                                         </div>
-                                        </div>
-                                        <div className="row">
-                                        <div className="input-field col s12">
-                                            <select id="cliente" className="select">
-                                            <option value="" disabled selected>Selecione..</option>
-                                            <option className="option"value="1">Cliente 1</option>
-                                            <option className="option" value="2">Cliente 2</option>
-                                            </select>
-                                            <label htmlFor="cliente">Cliente</label>
-                                        </div>
-                                        </div>
-                                      
-                                       
-                                       
                                         <div className="row">
                                             <div className=" col s12">
                                             <button className="btn " id="voltar" > 
-                                            <Link to="/produtos">Voltar</Link>
-                                           
+                                            <Link to="/servicos">Voltar</Link>
+                                        
                                             </button>
-                                            <button className="btn float" id="cadastrar"> Cadastrar</button>
+                                            <button className="btn float" id="cadastrar" onClick={clickMe}> Cadastrar</button>
                                             </div>
                                         </div>
-                                    
-                                        </form>
-                                    </div>
                                 
+                                    </form>
                                 </div>
-                                </div>
+                            
+                            </div>
                             </div>
                         </div>
-                
                     </div>
+            
                 </div>
             </div>
-           
-        )
+        </div>
         
-    }
+    )
+    
+
     
 }
 export default CadastroClienteProduto;
+
--- front/src/Components/servico/cadastroClienteServico.tsx
+++ front/src/Components/servico/cadastroClienteServico.tsx
@@ -1,161 +1,122 @@
-import React, {Component} from "react";
+import React, {useState, useEffect} from "react";
 import BarraNavegacao from "../barraNavegacao";
 import "../../Css/clientes.css"
 import { Link } from "react-router-dom";
 import { textChangeRangeIsUnchanged } from "typescript";
 import { render } from "@testing-library/react";
-import Selects from "./selects"
-type props = {
-    cliente: Array<any>,
-    servico: Array<any>
-}
-class CadastroClienteServico extends Component<{}, {clientes:Array<any>, servicos:Array<any>}>{
-    constructor(props: props | Readonly<props>) {
-        super(props)
-        this.state={
-            clientes:[],
-            servicos:[]
-        }
-        this.gerarCl = this.gerarCl.bind(this)
-        this.gerarSr = this.gerarSr.bind(this)
-    }
-    
-    gerarCl (clientes:Array<any>) {
-       
-        if (clientes.length <= 0) {
-            return <> </>
-        } else {
-            let lista = clientes.map( (n) =>
-              <option className="option" value={n.Id}> {n.Nome}</option>
-        )
-            return lista
-        }
-    }
-    gerarSr(servicos:Array<any>) {
-        
-        if (servicos.length <= 0) {
-            return <>0000</>
-        } else {
-            let lista = servicos.map( (n) =>
-            <option className="option" value={n.Id}>{n.Nome}</option>
-            )
-            return lista
-        }
-    }
-    listarServicos = () =>{
 
-        let dados: Array<any> = [];
+function CadastroClienteServico (){
+    
+    var cliente =[{Id:0, Nome:""}]
+    var servico =[{Id:0, Nome:""}]
+    const [clientes, setClientes] = useState(cliente)
+    const [servicos, setServicos] = useState(servico)
+    
+    var listarServicos = () =>{
         fetch("/listarServico", {
             method: "GET",
             headers: {
                 'Content-Type': 'application/json;charset=utf-8'
             },
         }).then((res)=> res.json()).then((data)=>{
-           
-            this.setState({
-                servicos:data
-               })
-               console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-               console.log(this.state.servicos)
-           
+            setServicos(data)
         })
-       
-        
     }
-    listarClientes =() =>{
+    var listarClientes =() =>{
         fetch("/listarClientes", {
             method: "get",
             headers: {
                 'Content-Type': 'application/json;charset=utf-8'
             },
         }).then((res)=> res.json()).then((data)=>{
-            
-           this.setState({
-            clientes:data
-           })
-           
+           setClientes(data )
         })
     }
    
-
-    componentDidMount() {
-        let select = document.querySelectorAll('.select');
-        M.FormSelect.init(select);
-         this.listarClientes();
-        this.listarServicos();
+    useEffect(()=>{
+        
+         listarClientes();
+         listarServicos();
+         let select = document.querySelectorAll('.select');
+         M.FormSelect.init(select);
+         setTimeout(() => {
+         let select = document.querySelectorAll('.select');
+         M.FormSelect.init(select);
+         }, 1000);
+    }, [])
+    
       
-    }
-    render(){
-        var botoes = [{valor: "ClIENTES", link:"/Clientes"}, {valor: "SERVIÇOS", link:"/servicos"},{valor: "PRODUTOS", link:"/produtos"} ]
-        let barraNavegacao = <BarraNavegacao  tema="purple accent-4" botoes={botoes} />;
-        return(
-            <div>
-               {barraNavegacao}
-              
-               <div className="container">
-                    <div className="row">
-                        <div className="col s12 ">
-                            <div className="card ">
-                                <div className="card-content ">
-                                <h1 className="card-title">CADASTRAR CLIENTE/SERVICO</h1>
-                                <hr></hr>
-                                <div className="card-body">
-                                    <div className="row">
-                                        <form className="col s12">
-                                       
-                                            <div className="row">
-                                                <div className="input-field col s12">
-                                                    <select id="servio" className="select">
-                                                    <option value="" disabled selected>Selecione..</option>
-                                                    {this.state.clientes.map((n)=>{
-                                                        return <option >{n.Nome}</option>
-                                                    })}
-                                                
-                                                    </select>
-                                                
-                                                    <label htmlFor="servico">Serviço</label>
-                                                </div>
-                                            
-                                            </div> 
-                                            <div className="row">
-                                            
-                                                <div className="input-field col s12">
-                                                    <select id="cliente" className="select">
-                                                    <option value="" disabled selected>Selecione..</option>
-                                                    
-                                                    {    this.state.servicos.map( (n) =>
-                                                    
-                                                        <option className="option" value={n.Id}>{n.Nome}</option>
-                                                        )}
-                                                    </select>
-                                                    <label htmlFor="cliente">Cliente</label>
-                                                </div>
-                                            </div>
-                                            <div className="row">
-                                                <div className=" col s12">
-                                                <button className="btn " id="voltar" > 
-                                                <Link to="/servicos">Voltar</Link>
-                                            
-                                                </button>
-                                                <button className="btn float" id="cadastrar"> Cadastrar</button>
-                                                </div>
-                                            </div>
+    
+    
+    var botoes = [{valor: "ClIENTES", link:"/Clientes"}, {valor: "SERVIÇOS", link:"/servicos"},{valor: "PRODUTOS", link:"/produtos"} ]
+    let barraNavegacao = <BarraNavegacao  tema="purple accent-4" botoes={botoes} />;
+    return(
+        <div>
+            {barraNavegacao}
+            
+            <div className="container">
+                <div className="row">
+                    <div className="col s12 ">
+                        <div className="card ">
+                            <div className="card-content ">
+                            <h1 className="card-title">CADASTRAR CLIENTE/PRODUTO</h1>
+                            <hr></hr>
+                            <div className="card-body">
+                                <div className="row">
+                                    <form className="col s12">
                                     
-                                        </form>
-                                    </div>
+                                        <div className="row">
+                                            <div className="input-field col s12">
+                                                <select id="servio" className="select">
+                                                <option value="" disabled selected>Selecione..</option>
+                                                {servicos.map(function (a) {
+                                                    return <option value={a.Id}> {a.Nome}</option>
+                                                })};
+                                            
+                                                </select>
+                                            
+                                                <label htmlFor="servico">Serviço</label>
+                                            </div>
+                                        
+                                        </div> 
+                                        <div className="row">
+                                        
+                                            <div className="input-field col s12">
+                                                <select id="cliente" className="select">
+                                                <option value="" disabled selected>Selecione..</option>
+                                                
+                                                {clientes.map(function (a) {
+                                                    return <option value={a.Id}> {a.Nome}</option>
+                                                })};
+                                                </select>
+                                                <label htmlFor="cliente">Cliente</label>
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className=" col s12">
+                                            <button className="btn " id="voltar" > 
+                                            <Link to="/servicos">Voltar</Link>
+                                        
+                                            </button>
+                                            <button className="btn float" id="cadastrar"> Cadastrar</button>
+                                            </div>
+                                        </div>
                                 
+                                    </form>
                                 </div>
-                                </div>
+                            
+                            </div>
                             </div>
                         </div>
-                
                     </div>
+            
                 </div>
             </div>
-           
-        )
+        </div>
         
-    }
+    )
+    
+
     
 }
 export default CadastroClienteServico;
